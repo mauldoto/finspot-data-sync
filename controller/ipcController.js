a77b69a11feae6f93c3507f22008b0f9270672ac
mauldoto <dev.mauldoto@gmail.com>
2025-04-17 14:53:21 +0700

    add new handler for add, update, and delete

--- controller/ipcController.js
+++ controller/ipcController.js
@@ -1,17 +1,118 @@
-const { ipcMain, dialog } = require('electron')
+const { ipcMain, dialog } = require("electron");
+const {
+  addEmployee,
+  updateEmployee,
+  deleteEmployee,
+} = require("./dbController");
 
 const openFileDialog = async () => {
-    const result = await dialog.showOpenDialog({
-      properties: ['openFile'],
-      filters: [{ name: 'Excel Files', extensions: ['xlsx', 'xls'] }]
-    });
-    return result.filePaths;
+  const result = await dialog.showOpenDialog({
+    properties: ["openFile"],
+    filters: [{ name: "Excel Files", extensions: ["xlsx", "xls"] }],
+  });
+  return result.filePaths;
+};
+
+const handleAddEmployee = async (event, dataExcel) => {
+  const errorLogs = [];
+  let successCount = 0;
+  let progressCount = 0;
+
+  for (const row of dataExcel) {
+    try {
+      await addEmployee(row);
+
+      successCount++;
+    } catch (err) {
+      errorLogs.push({
+        nip: row["NIP"],
+        kendala: err.message,
+      });
+    }
+
+    progressCount++;
+    event.sender.send(
+      "progress-update",
+      (progressCount / dataExcel.length) * 100
+    );
+  }
+
+  return {
+    total_success: successCount,
+    total_error: errorLogs.length,
+    errors: errorLogs,
   };
+};
 
-  const registerOpenDialogHandler = () => {
-    ipcMain.handle('dialog:openFile', openFileDialog);
+const handleUpdateEmployee = async (event, dataExcel) => {
+  const errorLogs = [];
+  let successCount = 0;
+  let progressCount = 0;
+
+  for (const row of dataExcel) {
+    try {
+      await updateEmployee(row);
+
+      successCount++;
+    } catch (err) {
+      errorLogs.push({
+        nip: row["NIP"],
+        kendala: err.message,
+      });
+    }
+
+    progressCount++;
+    event.sender.send(
+      "progress-update",
+      (progressCount / dataExcel.length) * 100
+    );
   }
 
-  module.exports = {
-    registerOpenDialogHandler
+  return {
+    total_success: successCount,
+    total_error: errorLogs.length,
+    errors: errorLogs,
+  };
+};
+
+const handleDeleteEmployee = async (event, dataExcel) => {
+  const errorLogs = [];
+  let successCount = 0;
+  let progressCount = 0;
+
+  for (const row of dataExcel) {
+    try {
+      await deleteEmployee(row);
+
+      successCount++;
+    } catch (err) {
+      errorLogs.push({
+        nip: row["NIP"],
+        kendala: err.message,
+      });
+    }
+
+    progressCount++;
+    event.sender.send(
+      "progress-update",
+      (progressCount / dataExcel.length) * 100
+    );
   }
+
+  return {
+    total_success: successCount,
+    total_error: errorLogs.length,
+    errors: errorLogs,
+  };
+};
+
+const registerIpcHandler = () => {
+  ipcMain.handle("dialog:openFile", openFileDialog);
+  ipcMain.handle("excel:add", handleAddEmployee);
+  ipcMain.handle("excel:update", handleUpdateEmployee);
+  ipcMain.handle("excel:delete", handleDeleteEmployee);
+};
+
+module.exports = {
+  registerIpcHandler,
+};
